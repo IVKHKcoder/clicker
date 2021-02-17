853231d1a4ae165be5795c167ef58df049f7cc4b
Viktoria Bugaeva <Viktoria Bugaeva vikyvv@mail.ru>
2021-02-17 14:54:41 +0200

    add function to buy

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 let points = 0;
 let houses = 0;
+let market = 0;
 let income = 0;
+let time = 0;
+
 
 function prss () {
 points++;
 console.log(points);
-change();
+change('value', points);
 }
 
 function buyhouse () {
 points -= 10;
 houses++;
-change();
-buyhous();
+change('doma', houses);
 }
 
-let change = () => {
-document.getElementById("value").innerHTML = points;
+let change = (id, product) => {
+document.getElementById(id).innerHTML = product;
 }
 
-let buyhous = () => {
-document.getElementById("doma").innerHTML = houses;
+// let buyhous = () => {
+// document.getElementById("doma").innerHTML = houses;
+// }
+
+// let bisnes = () => {
+// document.getElementById("bussines").innerHTML = bussiness;
+// }
+
+
+function bussines () {
+ houses -= 10;
+ market++;
+ change('bussines', market);
+	console.log(income);
 }
+
 
 let timerId = setTimeout(function tick() {
-  points++;
-  change();
-  timerId = setTimeout(tick, 2000);
-}, 2000)
+points++;
+change('value', points)
+change('doma', houses)
+	console.log(time)
+  timerId = setTimeout(tick, 1000);
+}, 1000)
 
